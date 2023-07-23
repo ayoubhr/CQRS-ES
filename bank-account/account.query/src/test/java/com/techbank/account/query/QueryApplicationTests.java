8bc02b779232eaba7c609c6a936c23dd1b7b99b3
ahr <ayoub_hrrr@hotmail.com>
2023-07-23 11:52:51 +0200

    basic setup completed

--- bank-account/account.query/src/test/java/com/techbank/account/query/QueryApplicationTests.java
+++ bank-account/account.query/src/test/java/com/techbank/account/query/QueryApplicationTests.java
@@ -1,10 +1,10 @@
-package com.techbank.account.cmd;
+package com.techbank.account.query;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class QueryApplicationTests {
 
 	@Test
 	void contextLoads() {
