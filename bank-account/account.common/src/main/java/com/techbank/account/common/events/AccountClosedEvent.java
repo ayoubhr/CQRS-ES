2876cefba539b59032a474045a473460b855a501
ahr <ayoub_hrrr@hotmail.com>
2023-07-30 21:00:25 +0200

    Queries and query dispatcher

--- bank-account/account.common/src/main/java/com/techbank/account/common/events/AccountClosedEvent.java
+++ bank-account/account.common/src/main/java/com/techbank/account/common/events/AccountClosedEvent.java
@@ -1,7 +1,6 @@
 package com.techbank.account.common.events;
 
 import com.techbank.cqrs.core.events.BaseEvent;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
