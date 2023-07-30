aafbb8b8453fd4fb862c5c38387ea1e2210de8fc
ahr <ayoub_hrrr@hotmail.com>
2023-07-30 20:05:29 +0200

    Command controllers implemented

--- cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/events/EventModel.java
+++ cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/events/EventModel.java
@@ -1,6 +1,7 @@
 package com.techbank.cqrs.core.events;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,6 +10,7 @@ import java.util.Date;
 
 @Data
 @SuperBuilder
+@NoArgsConstructor
 @Document(collection = "eventStore")
 public class EventModel {
     @Id
